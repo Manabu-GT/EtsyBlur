e9108e0a0d05ac3768d4be5d97047d938391a7b6
Manabu-GT <manabu1984+github@gmail.com>
2015-03-24 03:17:51 +0900

    fix: use android.support.v7.app.ActionBarDrawerToggle #3

--- sample/src/main/java/com/ms/square/android/etsyblurdemo/NavigationDrawerFragment.java
+++ sample/src/main/java/com/ms/square/android/etsyblurdemo/NavigationDrawerFragment.java
@@ -5,11 +5,11 @@ import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -140,7 +140,6 @@ public class NavigationDrawerFragment extends Fragment {
         mDrawerToggle = new EtsyActionBarDrawerToggle(
                 getActivity(),                    /* host Activity */
                 mDrawerLayout,                    /* DrawerLayout object */
-                R.drawable.ic_drawer,             /* nav drawer image to replace 'Up' caret */
                 R.string.navigation_drawer_open,  /* "open drawer" description for accessibility */
                 R.string.navigation_drawer_close  /* "close drawer" description for accessibility */
         ) {
